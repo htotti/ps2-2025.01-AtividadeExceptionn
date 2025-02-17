f5ef18d70d6291dd138034e2daa6db581f7efedd
Henrique Totti <henrique.totti.lapa@gmail.com>
2025-02-17 12:08:20 +0000

    atividade pra fazer

---
+++ src/br/dev/joaquim/exceptions/InsufficientFoundsException.java
@@ -0,0 +1,10 @@
+package br.dev.joaquim.exceptions
+
+public class InsufficientFoundsException extends Exception{
+    public InsufficientFoundsException(String message){
+        super(message);
+    }
+    
+}
+
+// throw new
